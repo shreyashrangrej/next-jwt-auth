0c51ff9123d2973150e3dc87765641e8a931ab83
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2023-03-09 18:13:20 +0530

    passport login still pending

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,3 +1,47 @@
+import prisma from "@/libs/prisma"
+import { NextResponse } from "next/server"
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken"
+import { use } from "next-api-middleware"
+import passport from "../../../util/passport"
+
+interface Login {
+    email: string,
+    password: string
+}
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+}
+
 export async function POST(request: Request) {
-    return new Response('Login Route')
+    return new Promise((resolve, reject) => {
+        passport.authenticate('local', { session: false }, async (err: any, user: any, info: any) => {
+            if (err) {
+                reject(err)
+            } else if (!user) {
+                resolve(NextResponse.json({ message: info.message }, { status: 401 }))
+            } else {
+                const body = await request.json()
+                const token = jwt.sign({ userId: user.email }, process.env.JWT_SECRET as string, {
+                    expiresIn: '7d',
+                })
+                try {
+                    const updatedUser = await prisma.user.update({
+                        where: {
+                            email: body.email,
+                        },
+                        data: {
+                            token: token,
+                        },
+                    });
+                    resolve(NextResponse.json({ token }));
+                } catch (err: any) {
+                    resolve(NextResponse.json(err.message));
+                }
+            }
+        })(request)
+    })
 }
