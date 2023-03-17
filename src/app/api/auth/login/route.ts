5e6e3d8962f8e1d7fe90c54355644f47c78c79ee
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2023-03-17 09:53:57 +0530

    removed passport

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,47 +1,24 @@
 import prisma from "@/libs/prisma"
-import { NextResponse } from "next/server"
-import bcrypt from 'bcrypt'
+import { NextRequest, NextResponse } from "next/server"
 import jwt from "jsonwebtoken"
-import { use } from "next-api-middleware"
-import passport from "../../../util/passport"
-
-interface Login {
-    email: string,
-    password: string
-}
-
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-}
 
 export async function POST(request: Request) {
-    return new Promise((resolve, reject) => {
-        passport.authenticate('local', { session: false }, async (err: any, user: any, info: any) => {
-            if (err) {
-                reject(err)
-            } else if (!user) {
-                resolve(NextResponse.json({ message: info.message }, { status: 401 }))
-            } else {
-                const body = await request.json()
-                const token = jwt.sign({ userId: user.email }, process.env.JWT_SECRET as string, {
-                    expiresIn: '7d',
-                })
-                try {
-                    const updatedUser = await prisma.user.update({
-                        where: {
-                            email: body.email,
-                        },
-                        data: {
-                            token: token,
-                        },
-                    });
-                    resolve(NextResponse.json({ token }));
-                } catch (err: any) {
-                    resolve(NextResponse.json(err.message));
-                }
-            }
-        })(request)
+    const body = await request.json()
+    const token = jwt.sign({ userId: body.email }, process.env.JWT_SECRET as string, {
+        expiresIn: '7d',
     })
+    try {
+        const updatedUser = await prisma.user.update({
+            where: {
+                email: body.email,
+            },
+            data: {
+                token: token,
+            },
+        });
+        return (NextResponse.json({ token }));
+    } catch (err: any) {
+        return (NextResponse.json(err.message));
+    }
 }
+
